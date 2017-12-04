873991277b7e1c6c6eabdf9b2deb9d0f329dbf92
JinXiLi-15 <34221636+JinXiLi-15@users.noreply.github.com>
2017-12-03 21:45:30 -0500

    Update card.cc

--- card.cc
+++ card.cc
@@ -1,4 +1,7 @@
+#include "card.h"
+
 void Card::modify(int Ach, int Lch){return;}
 void Card::mofifyAct(int mod) {return;}
 bool Card::acting() {return true;}
 void Card::setAC(){return;}
+int Card::getAblCost(){return 0;}
