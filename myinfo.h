c4b5152fecf8750fb5b68e60a4a7d95854fa7726
Harris Yang <harrisyang611@outlook.com>
2017-12-03 19:39:16 -0500

    Update myinfo.h

--- myinfo.h
+++ myinfo.h
@@ -1,18 +1,17 @@
 #ifndef __MYINFO_H__
 #define __MYINFO_H__
-
-#include <vector>
-#include "card.h"
+#include <string>
+#include "player.h"
 #include "CardInfo.h"
 
 struct MyInfo{
 	std::string pName = "";
 	int life=20;
 	int mana=3;
-	std::vector<CardInfo> pHand=nullptr;
-	std::vector<CardInfo> pBoard=nullptr;
-	Card* pGYard=nullptr;
-	Card* pRitual=nullptr;
+	std::vector<CardInfo> pHand;
+	std::vector<CardInfo> pBoard;
+	CardInfo pGYard;
+	CardInfo pRitual;
 };
 
 #endif
